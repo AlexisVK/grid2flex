68483689d709fc64648f5d46d50b3ad583cd9521
Aleksey Chegodaev <bezbam@gmail.com>
2015-12-08 17:53:39 +0200

    fixes for alignment; added configuration features

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function() {
-    gulp.src('src/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));
+	gulp.src('src/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
+		.pipe(gulp.dest('./dist/'));
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('src/**/*.scss',['styles']);
+	gulp.watch('src/**/*.scss',['styles']);
 });
